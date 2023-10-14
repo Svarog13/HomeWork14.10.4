b854e9a79233eadfd0ca42212e7bfd90751f76b2
lenovo <lenovo@DESKTOP-D0T6Q1U>
2023-10-14 21:09:37 +0300

    Add project files.

---
+++ HomeWork14.10.4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+Console.WriteLine("Enter the digit for Factorial: ");
+int digit = int.Parse(Console.ReadLine());
+int factorial = 1;
+
+if (digit < 0)
+{
+    Console.WriteLine("Factorial don't work with negative digit!");
+}
+else
+{
+    for (int i = 1; i <= digit; i++)
+    {
+        factorial *= i;
+    }
+}
+
+Console.WriteLine($"Factorial of digit {digit} = {factorial}");
